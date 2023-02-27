4cc8908d0cf77d5e5c7db19eac53318ef8ef33e8
Zach Beiser <zbeiser@gmail.com>
2023-02-27 15:58:14 -0800

    added User schema

---
+++ models/User.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose');
+const thoughtSchema = require('./Thought')
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please enter a valid email'],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
