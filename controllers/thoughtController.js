5031eba3a354ea4b2a2b84fea1ab634e9faa64ba
Zach Beiser <zbeiser@gmail.com>
2023-02-28 15:53:13 -0800

    wrote addReaction and deleteReaction

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -62,6 +62,31 @@ module.exports = {
       .catch((err) => res.status(500).json(err));
   },
   // Create a reaction
-
+  addReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $addToSet: { reactions: req.body } },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: 'No thought with that ID' })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
   // Delete a reaction
+  deleteReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $pull: { reactions: { reactionId: req.params.reactionId } } },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: 'No thought with that ID' })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 };
