56853e80e87a45c88d65ce098d3666acad69a003
Zach Beiser <zbeiser@gmail.com>
2023-02-28 11:21:40 -0800

    wrote get all users route

---
+++ controllers/userController.js
@@ -0,0 +1,25 @@
+const { User, Thought } = require('../models');
+
+// Aggregate function to get the total number of users
+const totalCount = async () =>
+  User.aggregate()
+    .count('userCount')
+    .then((numberOfUsers) => numberOfUsers);
+
+module.exports = {
+  // Get all users
+  getUsers(req, res) {
+    User.find()
+      .then(async (users) => {
+        const userObj = {
+          users,
+          totalCount: await totalCount(),
+        };
+        return res.json(userObj);
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+};
