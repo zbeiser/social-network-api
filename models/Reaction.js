535b5b50c922854f463735e4da172691bed2f72b
Zach Beiser <zbeiser@gmail.com>
2023-02-27 17:12:42 -0800

    added thought schema

---
+++ models/Reaction.js
@@ -0,0 +1,15 @@
+const { Schema, Types } = require('mongoose');
+
+const reactionSchema = new Schema(
+  {
+    
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = reactionSchema;
