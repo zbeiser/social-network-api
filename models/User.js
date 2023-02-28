799758d4ff39e37e6e2089a2b36570ccf68d85d2
Zach Beiser <zbeiser@gmail.com>
2023-02-27 16:02:04 -0800

    added friendCount virtual to User schema

--- models/User.js
+++ models/User.js
@@ -36,6 +36,11 @@ const userSchema = new Schema(
   }
 );
 
+userSchema.virtual('friendCount').get(function()
+{
+  return this.friends.length;
+});
+
 const User = model('user', userSchema);
 
 module.exports = User;
