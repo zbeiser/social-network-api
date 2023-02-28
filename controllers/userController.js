633b4c5fd93a037a5bc34e83f9f73a3924de85d6
Zach Beiser <zbeiser@gmail.com>
2023-02-28 14:06:22 -0800

    wrote userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,4 +22,66 @@ module.exports = {
         return res.status(500).json(err);
       });
   },
+  // Get a single user
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .then(async (user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json({user})
+      )
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+  // Create a new user
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Update a user
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { users: req.params.userId },
+      { $set: { username: req.body.username, email: req.body.email } },
+      { new: true }
+    )
+    .then((user) =>
+        !user
+          ? res.status(404).json({
+              message: 'No such user exists',
+            })
+          : res.json({ message: 'User successfully dupdated' })
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  // Delete a user and associated thoughts
+  deleteUser(req, res) {
+    User.findOneAndRemove({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No such user exists' })
+          : Thought.findOneAndUpdate(
+              { users: req.params.userId },
+              { $pull: { users: req.params.userId } },
+              { new: true }
+            )
+      )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({
+              message: 'User deleted, but no thoughts found',
+            })
+          : res.json({ message: 'User successfully deleted' })
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
 };
