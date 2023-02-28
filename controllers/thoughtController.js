73857c5537e2e628171e0da50f8652bb43f9a3a5
Zach Beiser <zbeiser@gmail.com>
2023-02-28 15:08:07 -0800

    wrote getThoughts, getSingleThought, createThought, and updateThought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,1 +1,48 @@
 const { User, Thought } = require('../models');
+
+module.exports = {
+  // Get all thoughts
+  getThoughts(req, res) {
+    Thought.find()
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Get a single thought
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: 'No thought with that ID' })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // Create a new thought
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then((thought) => res.json(thought))
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+  // Update a thought
+  updateThought(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((course) =>
+        !course
+          ? res.status(404).json({ message: 'No course with this id!' })
+          : res.json(course)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // Delete a thought
+
+  // Create a reaction
+
+  // Delete a reaction
+};
