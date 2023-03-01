006c35b2939e77ca0520e4b2ab3bf9e676e2eea5
Zach Beiser <zbeiser@gmail.com>
2023-02-28 16:13:51 -0800

    added .toArray() method to getUsers and getThoughts

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -3,7 +3,7 @@ const { User, Thought } = require('../models');
 module.exports = {
   // Get all thoughts
   getThoughts(req, res) {
-    Thought.find()
+    Thought.find().toArray()
       .then((thoughts) => res.json(thoughts))
       .catch((err) => res.status(500).json(err));
   },
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,26 +1,11 @@
 const { User, Thought } = require('../models');
 
-// Aggregate function to get the total number of users
-const totalCount = async () =>
-  User.aggregate()
-    .count('userCount')
-    .then((numberOfUsers) => numberOfUsers);
-
 module.exports = {
   // Get all users
   getUsers(req, res) {
-    User.find()
-      .then(async (users) => {
-        const userObj = {
-          users,
-          totalCount: await totalCount(),
-        };
-        return res.json(userObj);
-      })
-      .catch((err) => {
-        console.log(err);
-        return res.status(500).json(err);
-      });
+    User.find().toArray()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
   },
   // Get a single user
   getSingleUser(req, res) {
